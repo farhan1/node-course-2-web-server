78bdefe6112ede67f1976ccf2f63ccb63c98694a
root <root@Farhan-PC.localdomain>
2017-10-24 20:36:50 +0100

    test commit

--- server.js
+++ server.js
@@ -10,19 +10,20 @@ app.set('view engine', hbs);
 
 app.use((req, res, next) => {
   var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
+  var log = `${now} ${req.method} ${req.url}`;
+
   console.log(log);
   fs.appendFile('server.log', log + '\n', (err) => {
     if (err) {
-      console.log('unable to append to server.log');
+      console.log("Unable to append to server.log");
     }
   });
   next();
 });
 
-// app.use((req, res, next) => {
-//   res.render('maintenance.hbs');
-// });
+// app.use((trq, res, next) => {
+//     res.render('maintenance.hbs');
+// })
 
 app.use(express.static(__dirname + '/public'));
 
